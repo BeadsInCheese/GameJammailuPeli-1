defcc909c94233a148dd94057977362ef8968c44
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2022-10-15 21:01:17 +0300

    added CharacterController

--- Assets/Player/Scripts/CharacterControl.cs
+++ Assets/Player/Scripts/CharacterControl.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterControl : MonoBehaviour
 {
     // Start is called before the first frame update
+    PlayerInput playerInput;
     void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = playerInput.actions["walk"].ReadValue<Vector2>();
+        transform.position = new Vector2(movement.x*Time.deltaTime+transform.position.x,movement.y*Time.deltaTime+transform.position.y);
     }
 }
