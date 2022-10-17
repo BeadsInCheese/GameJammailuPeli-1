eea6bc8407abaf28d508441e324dd2d0110eee4b
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2022-10-17 17:25:33 +0300

    BTtree update

--- Assets/Common/Scripts/BTTree/BTTree.cs
+++ Assets/Common/Scripts/BTTree/BTTree.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree : Node
+public class BTTree : Node
 {
-    public Tree()
+    public BTTree()
     {
         name="Tree";
     }
