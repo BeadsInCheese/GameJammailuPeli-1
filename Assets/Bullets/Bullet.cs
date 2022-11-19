3d91b41f83820f2de9124808b772435a1db4263c
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2022-11-19 18:38:03 +0200

    Added BulletTower

--- Assets/Bullets/Bullet.cs
+++ Assets/Bullets/Bullet.cs
@@ -7,9 +7,19 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     public Vector2 direction;
     public float speed=5;
+    public int dmg=20;
     void Start()
     {
         transform.GetComponent<Rigidbody2D>().velocity=direction*speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            CharacterControl.instance.stats.ChangeHP(-dmg);
+
+        }
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
--- Assets/Enemies/ShootingTower/TowerBehaviour.cs
+++ Assets/Enemies/ShootingTower/TowerBehaviour.cs
@@ -8,8 +8,9 @@ public class TowerBehaviour : MonoBehaviour
     public GameObject bulletPrefab;
     float sight = 10;
     float attackRange = 5;
-    public GameObject targetObject;
+     GameObject targetObject;
     public Vector2 target;
+    public float maxCooldown = 0.5f;
     // Start is called before the first frame update
     public Node.Status IsInRange() {
         if ((target - (Vector2)(transform.position)).magnitude < attackRange) {
@@ -20,18 +21,24 @@ public class TowerBehaviour : MonoBehaviour
             return Node.Status.SUCCESS;
         }
     }
+    private float cooldown;
     public Node.Status Shoot()
     {
-        Vector2 dir = (target - (Vector2)(transform.position)).normalized;
-        var bf=Instantiate(bulletPrefab);
-        bf.transform.position = transform.position+new Vector3(dir.x , dir.y,transform.position.z);
-        var bullet=bf.GetComponent<Bullet>();
-        bullet.direction = dir;
-
+        if (cooldown <= 0)
+        {
+            cooldown = maxCooldown;
+            Vector2 dir = (target - (Vector2)(transform.position)).normalized;
+            var bf = Instantiate(bulletPrefab);
+            bf.transform.position = transform.position + new Vector3(dir.x, dir.y, transform.position.z);
+            var bullet = bf.GetComponent<Bullet>();
+            bullet.direction = dir;
+        }
         return Node.Status.SUCCESS;
     }
     void Start()
     {
+        targetObject = CharacterControl.instance.gameObject;
+        cooldown = 0;
         Brain = new BTTree();
         SelectorNode actionSelector = new SelectorNode("actionSelector");
         LeafNode isInRange = new LeafNode("IsInRange", IsInRange);
@@ -46,6 +53,10 @@ public class TowerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
         target = targetObject.transform.position;
         Brain.Process();
     }
